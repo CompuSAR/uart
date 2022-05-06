5a50233a4c36ef70d6e3ae298cf07a7078b391d3
Shachar Shemesh <shachar@shemesh.biz>
2022-05-06 03:51:37 +0300

    Project template

---
+++ uart.srcs/sources_1/new/uart_send.sv
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/06/2022 03:51:06 AM
+// Design Name: 
+// Module Name: uart_send
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module uart_send(
+    input [7:0] data_in,
+    input data_in_ready,
+    output receive_ready,
+    input clock
+    );
+endmodule
